23d3e0f812e18a7db119fa0e92affe7b0a5459cc
uztbt <yujitabata@icloud.com>
2021-09-12 15:37:39 +0900

    Prevent quick reload from the same IP address.

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,23 @@
+import http from 'http';
+
 type ExecutionMode = 'debug' | 'release';
+
 export const executionModes: ReadonlyArray<ExecutionMode> = ['debug', 'release'];
 
 export function getConfig(mode: ExecutionMode) {
   const common = {
     players: 2,
-    maxRooms: 5
+    maxRooms: 10,
+    allowReloadInterval: 2000
   };
   const debug = {
     port: 8081,
     socketIOOpts: {
       cors: {
         origin: "http://localhost:8080",
-        methods: ["GET", "POST"]
-      }
+        methods: ["GET"],
+      },
+      allowRequest: preventQuickReload
     }
   };
   const release = {
@@ -20,8 +25,9 @@ export function getConfig(mode: ExecutionMode) {
     socketIOOpts: {
       cors: {
         origin: "https://yuji.page",
-        methods: ["GET", "POST"]
-      }
+        methods: ["GET"]
+      },
+      allowRequest: preventQuickReload
     }
   };
 
@@ -32,3 +38,30 @@ export function getConfig(mode: ExecutionMode) {
       return Object.assign({}, common, release);
   }
 };
+
+
+const remoteAddressCache: Record<string, Date> = {};
+
+function preventQuickReload(req: http.IncomingMessage, callback: (err: string | null | undefined, success: boolean) => void): void {
+  console.log(`
+    req.socket.remoteAddress = ${JSON.stringify(req.socket.remoteAddress)}
+    req.socket.remoteFamily = ${req.socket.remoteFamily}
+    `);
+  const remoteAddress = req.socket.remoteAddress;
+  if (typeof remoteAddress === "undefined") {
+    return callback(null, true);
+  }
+  const lastAccess = remoteAddressCache[remoteAddress];
+  const now = new Date();
+  if (typeof lastAccess === "undefined") {
+    remoteAddressCache[remoteAddress] = now;
+    return callback(null, true);
+  }
+  if (now.valueOf() - lastAccess.valueOf() > getConfig("debug").allowReloadInterval){
+    remoteAddressCache[remoteAddress] = now;
+    return callback(null, true);
+  } else {
+    console.log(`Rejecting connection due to a quick reload. now = ${now}, lastAccess = ${lastAccess}`);
+    return callback(null, false);
+  } 
+}
